8fcc4b4b7d9ad6582ff071c1fec36f877e4473a9
Claudiana Ferreira <tech.claudiana@gmail.com>
2025-11-26 23:06:55 -0300

    melhorias no cenario de teste inventario de produtos do site

--- tests/specs/inventory.spec.ts
+++ tests/specs/inventory.spec.ts
@@ -1,24 +1,20 @@
 import { test } from '../helpers/testSetup';
 import { expect } from '@playwright/test';
-import { Inventory } from '../pages/productsInventory';
 
 test.describe('Inventário de produtos do site ecomm', () => {
-
-    test('Deve contabilizar os produtos adicionados no carrinho de compra do usuário logado', 
-    async ({ loggedUser }) => {
-        let inventory: Inventory;
-
+    
+    test('Deve adicionar os produtos no carrinho de compra', 
+    async ({ loggedUser, inventoryPage }) => { 
+        
         await test.step('Dado que o usuário está logado e na página de Inventário', async () => {
-            inventory = new Inventory(loggedUser);
         });
 
         await test.step('Quando o usuário adiciona os três produtos no carrinho', async () => {
-            await inventory.adicionarProdutosAoCarrinho();
+            await inventoryPage.adicionarProdutosAoCarrinho();
         });
 
         await test.step('Então a contagem do carrinho deve ser igual à 3', async () => {
-            await expect(inventory.carrinhoIcone).toHaveText('3'); 
+            await expect(inventoryPage.carrinhoIcone).toHaveText('3'); 
         });
     });
-    
 });
