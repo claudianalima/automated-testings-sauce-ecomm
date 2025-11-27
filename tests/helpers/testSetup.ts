fb413aad2874385fcad5cbe7bd88ce4f7421a69c
Claudiana Ferreira <tech.claudiana@gmail.com>
2025-11-26 23:04:30 -0300

    melhorias no código do test setup

--- tests/helpers/testSetup.ts
+++ tests/helpers/testSetup.ts
@@ -1,36 +1,34 @@
 import { test as baseTest, Page, expect } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
+import { Inventory } from '../pages/productsInventory';
 import { INVENTORY_DATA } from '../testdata/dataInventory';
 import { URL_BASE } from '../../src/utils/env-utils';
 
 type MyFixtures = {
     loginPageOnly: LoginPage; 
+    inventoryPage: Inventory;
+    
     loggedUser: Page;
-    loggedUserIvalid: Page; 
 };
 
 export const test = baseTest.extend<MyFixtures>({
     
-    loginPageOnly: [
-        async ({ page }, use) => {
-            const loginPage = new LoginPage(page);
-            await use(loginPage); 
-        },
-        { scope: 'test' }
-    ],
+    loginPageOnly: async ({ page }, use) => {
+        const loginPage = new LoginPage(page);
+        await use(loginPage); 
+    },
+    
+    inventoryPage: async ({ page }, use) => {
+        const inventoryPage = new Inventory(page);
+        await use(inventoryPage); 
+    },
 
-    loggedUser: [
-        async ({ page, loginPageOnly }, use) => {
-            
-            await loginPageOnly.goto(); 
-            await loginPageOnly.loginSucesso();
-
-            const fullInventoryUrl = `${URL_BASE}${INVENTORY_DATA.URL_INVENTARIO_PRODUTOS}`;
-
-            await expect(loginPageOnly.page).toHaveURL(fullInventoryUrl); 
-
-            await use(page); 
-        },
-        { scope: 'test' }
-    ]
+    loggedUser: async ({ page, loginPageOnly }, use) => {
+        await loginPageOnly.loginSucesso();
+        
+        const fullInventoryUrl = `${URL_BASE}${INVENTORY_DATA.URL_INVENTARIO_PRODUTOS}`;
+        await expect(page).toHaveURL(fullInventoryUrl); 
+        
+        await use(page); 
+    },
 });
